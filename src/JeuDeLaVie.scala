7ddf1f79d2f5b7367abe3b632f53abac0de1180f
Michel Grolet <michelgrolet@gmail.com>
2022-03-24 09:50:58 +0100

    Update JeuDeLaVie.scala

--- src/JeuDeLaVie.scala
+++ src/JeuDeLaVie.scala
@@ -2,18 +2,20 @@ import java.util.NoSuchElementException
 
 object JeuDeLaVie {
 	def main(args: Array[String]): Unit = {
-		val l = List(
+		val l = chaineToGrille(List(
 			"  X  ",
 			" XXX ",
 			"XX XX",
 			" XXX ",
 			"  X  "
-		)
-		val v = chaineToGrille(l)
-		println(v)
-		afficherGrille(v)
-		val g: Grille = survivantes(v)
-		afficherGrille(g)
+		))
+		val l2 = chaineToGrille(List(
+			" X ",
+			"  X",
+			"XXX"
+		))
+
+		jeuDeLaVie(l, 4)
 	}
 
 	type Grille = List[(Int,Int)]
@@ -47,67 +49,72 @@ object JeuDeLaVie {
 
 	//q2
 	def afficherGrille(g:Grille):Unit = {
-		// find minimal couple in the grid
-		val min = g reduceLeft ((a,b) => {(
-			if (a._1 < b._1) a._1 else b._1,
-			if (a._2 >= b._2) b._2 else a._2
-		)})
+		if (g.isEmpty)
+			println("(vide)")
+		else {
+			// couple minimal de la grille
+			val min = g reduceLeft ((a, b) =>
+				(if (a._1 < b._1) a._1 else b._1,
+				if (a._2 >= b._2) b._2 else a._2))
 
-		val max = g reduceLeft ((a,b) => {(
-			if (a._1 < b._1) b._1 else a._1,
-			if (a._2 > b._2) a._2 else b._2
-		)})
+			// couple maximal de la grille
+			val max = g reduceLeft ((a, b) =>
+				(if (a._1 < b._1) b._1 else a._1,
+				if (a._2 > b._2) a._2 else b._2))
 
-		// cherche c dans g
-		def grilleContient(c:(Int,Int)): Boolean = {
-			(g filter (curr => curr==c)) != Nil
-		}
+			// cherche c dans g
+			def grilleContient(c: (Int, Int)): Boolean = !(g filter (curr => curr == c)).isEmpty
 
-		// itère de min à max et affiche quand le couple est dans g
-		// ._1 : ligne concernée
-		// ._2 : colonne concernée
-		def iterer(c:(Int,Int)): Unit = {
-			// si il reste des lignes :
-			if (c._1<=max._1) {
-				// si il reste des colonnes :
-				if (c._2<=max._2) {
-					// si c est en X dans la grille :
-					if (grilleContient(c)) print("X")
-					else print(" ")
-					iterer((c._1, c._2+1))
-				} else { // ligne suivante
-					println("")
-					iterer((c._1+1, 0))
+			// itère de min à max et affiche quand le couple est dans g
+			// ._1 : ligne concernée
+			// ._2 : colonne concernée
+			def iterer(c: (Int, Int)): Unit = {
+				// si il reste des lignes :
+				if (c._1 <= max._1) {
+					// si il reste des colonnes :
+					if (c._2 <= max._2) {
+						// si c est en X dans la grille :
+						if (grilleContient(c)) print(" X ")
+						else print("   ")
+						iterer((c._1, c._2 + 1))
+					} else { // ligne suivante
+						println("")
+						iterer((c._1 + 1, 0))
+					}
 				}
 			}
+			iterer(min)
 		}
-
-		iterer(min)
 	}
 
 	//q3
-	def voisines8(l:Int,c:Int):List[(Int, Int)] = {
+	def voisines8(l:Int,c:Int):List[(Int, Int)] =
 		(l-1,c-1)::(l-1,c)::(l-1,c+1)::(l,c-1)::(l,c+1)::(l+1,c-1)::(l+1,c)::(l+1,c+1)::Nil
-	}
+
 
 	//q4
-	def survivantes(g:Grille):Grille=g match{
-		case Nil=>Nil
-		case t::q=>
-			val voisines=compte(g,voisines8(t._1,t._2))
-			if(voisines>2 && voisines<3){
-				t::survivantes(q)
-			}
-			else survivantes(q)
-	}
+	def listeVoisinesVivantes(a:Int, b:Int, g:Grille):Grille = voisines8(a, b) filter ((va, vb)=>g.contains((va, vb)))
+	def survivantes(g:Grille):Grille =
+		g filter((ord, abs) =>
+			val length = listeVoisinesVivantes(ord, abs, g).length
+			length == 2 || length == 3)
 
-	def compte(g:Grille,vois:Grille):Int=vois match{
-		case Nil => 0
-		case t::q=>
-			val l=g filter(x=>x==t)
-			if(l!=Nil) 1+compte(g,q)
-			else compte(g,q)
-	}
+	//q5
+	def listeVoisinesMortes(a:Int, b:Int, g:Grille):Grille = voisines8(a, b) filter ((va, vb)=>(!g.contains((va, vb))))
+	def candidates(g:Grille):Grille =
+		(g foldLeft List.empty)((acc, elem)=> acc++listeVoisinesMortes(elem._1, elem._2, g))
 
-	//def candidates(g:Grille)
+	//q6
+	def naissances(g:Grille):Grille =
+		candidates(g) filter((ord, abs) => listeVoisinesVivantes(ord, abs, g).length == 3)
+
+    //q7
+	def jeuDeLaVie(init:Grille, n:Int):Unit =
+		if (n>0) {
+			afficherGrille(init)
+			println(init)
+			println("\n-=--=--=--=--=--=--=--=--=-\n")
+			// TODO : concaténer les deux listes en dessous au lieu de faire ++
+			jeuDeLaVie(naissances(init)++survivantes(init), n-1)
+		}
 }
